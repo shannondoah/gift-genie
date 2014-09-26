e1615dddee8cf8cee9bc6528e79955992632b546
Thiago Nogueira <nogueirathiago@gmail.com>
2014-09-26 01:29:35 +0000

    Index and signup form V1

--- sinatra-skeleton-master/app/actions.rb
+++ sinatra-skeleton-master/app/actions.rb
@@ -2,13 +2,21 @@
 # helper do
 
 # end
+enable :sessions
 
 #GET ACTIONS
 # Homepage (Root path)
 get '/' do
-  @users = User.all
+  @user = User.new
+  @categories = Category.all
   @products = Product.all
   erb :index
+
+end
+#Shows a signup form to create a new user.
+get '/users/new' do 
+  @user = User.new
+  erb :'users/new'
 
 end
 
@@ -33,13 +41,6 @@ get '/products/new' do
 
 end
 
-#Shows a signup form to create a new user.
-get 'users/new' do 
-  
-  erb :'users/new'
-
-end
-
 #Form to edit user profile.
 get 'users/:id/edit' do 
 
@@ -47,9 +48,45 @@ get 'users/:id/edit' do
 
 end
 
-#POST ACTIONS
-post 'users/new' do 
+get '/logout' do 
+  session[:user_id] = nil
+  redirect '/'
+end
 
+#POST ACTIONS
+post '/login' do 
+  @user = User.where(
+    email:   params[:email],
+    password: params[:password]
+    ).first
+  if @user 
+    session[:user_id] = @user.id
+    redirect '/users/show' 
+  else
+    redirect '/'
+  end
+end
+
+post '/users/new' do 
+  @user = User.new(
+    email: params[:email],
+    password: params[:password],
+    name: params[:name],
+    avatar: params[:avatar],
+    cover_photo: params[:cover_photo],
+    male: params[:male],
+    female: params[:female],
+    birthdate: params[:birthdate],
+    shipping_address: params[:shipping_address],
+    phone: params[:phone]
+    )
+  if @user.save
+    session[:user_id] = @user_id
+    redirect '/users/:id'
+  else
+    redirect '/users/new'
+    
+  end
   erb :'users/new'
 
 end
