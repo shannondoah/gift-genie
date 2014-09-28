71651ced8c9bff95967c237b439e6243a10ad28a
pandabeat <chris.chan.is@gmail.com>
2014-09-28 02:27:07 +0000

    random product poof works

--- sinatra-skeleton-master/app/models/product.rb
+++ sinatra-skeleton-master/app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ActiveRecord::Base
+
   has_and_belongs_to_many :categories
   has_many :favourites
+
+  # validates :name, presence: true, length: { maximum: 40 }
+  # validates :description, presence: true, length: { maximum: 140 }
 end
--- sinatra-skeleton-master/public/javascript/application.js
+++ sinatra-skeleton-master/public/javascript/application.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
+	$('#twitter-button').click(function(event) {
+   
+  	}
 
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
 });
+
