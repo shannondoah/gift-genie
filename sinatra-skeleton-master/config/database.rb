5d8eaf675d1b32c4a33cf8f78224769ed9805569
pandabeat <chris.chan.is@gmail.com>
2014-10-10 21:53:33 +0000

    configure for deployment

--- sinatra-skeleton-master/config/database.rb
+++ sinatra-skeleton-master/config/database.rb
@@ -1,15 +1,28 @@
 configure do
-  # Log queries to STDOUT in development
-  if Sinatra::Application.development?
-    ActiveRecord::Base.logger = Logger.new(STDOUT)
+  ActiveRecord::Base.logger = Logger.new(STDOUT) if Sinatra::Application.development?
 
+  configure :development, :test do
     set :database, {
-    adapter: "sqlite3",
-    database: "db/db.sqlite3"
-  }
-  else
-    set :database, ENV['DATABASE_URL']
+      adapter: "sqlite3",
+      database: "db/db.sqlite3"
+    }
   end
+
+  configure :production do
+    db = URI.parse(ENV['DATABASE_URL'])
+
+    ActiveRecord::Base.establish_connection(
+     :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+     :host     => db.host,
+     :username => db.user,
+     :password => db.password,
+     :database => db.path[1..-1],
+     :encoding => 'utf8'
+    )
+  end
+
+  # Load all models from app/models, using autoload instead of require
+
 
   # Load all models from app/models, using autoload instead of require
   # See http://www.rubyinside.com/ruby-techniques-revealed-autoload-1652.html
@@ -17,7 +30,30 @@ configure do
     filename = File.basename(model_file).gsub('.rb', '')
     autoload ActiveSupport::Inflector.camelize(filename), model_file
   end
-
 end
+
+
+
+# configure do
+#   # Log queries to STDOUT in development
+#   if Sinatra::Application.development?
+#     ActiveRecord::Base.logger = Logger.new(STDOUT)
+
+#     set :database, {
+#     adapter: "sqlite3",
+#     database: "db/db.sqlite3"
+#   }
+#   else
+#     set :database, ENV['DATABASE_URL']
+#   end
+
+#   # Load all models from app/models, using autoload instead of require
+#   # See http://www.rubyinside.com/ruby-techniques-revealed-autoload-1652.html
+#   Dir[APP_ROOT.join('app', 'models', '*.rb')].each do |model_file|
+#     filename = File.basename(model_file).gsub('.rb', '')
+#     autoload ActiveSupport::Inflector.camelize(filename), model_file
+#   end
+
+# end
 
 
